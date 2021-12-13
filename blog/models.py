8e1c376caaf71130a14729593bb5f427faf181ed
anurag629 <akvermaav629@gmail.com>
2021-12-14 01:16:26 +0530

    added first time

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
